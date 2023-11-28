218ada4f6a84ba8a0378ebea2e42ccab4aac5fe4
SeniorCub <farinderif@gmail.com>
2023-11-28 10:48:25 +0100

    Assignment No 1 in Manual
    I just started a new semester to learn Python and C language

---
+++ 200 Level 2nd Semester/CYB 204/AssignmentOne.py
@@ -0,0 +1,41 @@
+# Page 89
+
+# 1
+x = 2 + 9*((3 * 12) - 8) / 10
+print(x)
+
+# 2
+# Banana = mango
+
+# 3
+if (9 < 0) and (0 < -9):
+    print("Hello")
+elif (9 > 0) or False:
+    print("Good")
+else:
+    print("Bad")
+
+# 4
+d = {0: 'a', 1: 'b', 2: 'c'}
+for x in d.keys():
+    print(d[x])
+
+# 5
+# print("hello" +1+2+3)
+
+# 6
+print("xyyzxyzxzxyy".count('yy'))
+
+# 7
+print("abcdef".find("cd"))
+
+# 8
+list1 = [0.5 * x for x in range(0,4)]
+print(list1)
+
+# 9
+print('Ab!2'.swapcase())
+
+# 10
+list2 = [2, 33, 222, 14, 25]
+print(list2[-1])
