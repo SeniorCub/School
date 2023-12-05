56dd99a89a2ec5594225402a952a9230ccd137b2
SeniorCub <farinderif@gmail.com>
2023-12-05 10:49:17 +0100

    Create main.c
    C basic- variables and data types

---
+++ 200 Level 2nd Semester/CSC202/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+     int age = 20 // integer - %d or %i
+     double gpa = 4.52 // double - %lf
+     char grade = "A" // character - %c
+     char name[15] = "Farinde Reuben" // string - %s
+
+     printf("Name: %s%S\n Age: %d\n Gpa: %f\n Grade: %c\n");
+
+     return 0;
+}
