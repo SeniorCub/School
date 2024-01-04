3525c6796055cc836d7e875bc6adced008495f2d
SeniorCub <farinderif@gmail.com>
2024-01-04 13:14:56 +0100

    trying to run c language but not working

--- 200 Level 2nd Semester/CSC202/main.c
+++ 200 Level 2nd Semester/CSC202/main.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <studio.h>
 
-int main(){
-
-     int age = 20 // integer - %d or %i
-     double gpa = 4.52 // double - %lf
-     char grade = "A" // character - %c
-     char name[15] = "Farinde Reuben" // string - %s
-
-     printf("Name: %s%S\n Age: %d\n Gpa: %f\n Grade: %c\n");
+int main()
+{
+     /*my first program in c */
+     printf("Hello, World! \n");
 
      return 0;
 }
