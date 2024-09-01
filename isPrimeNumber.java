17f651be49133a12b97eecfe0f0a2ecb7a2d9dcf
Reuben Ifeoluwa Farinde <99650027+SeniorCub@users.noreply.github.com>
2024-09-01 20:02:12 +0100

    this checks if a number is prime or not without importing any external thing

---
+++ isPrimeNumber.java
@@ -0,0 +1,27 @@
+public class PrimeChecker {
+
+    public static void main(String[] args) {
+        // Example number to check
+       int number = 2;
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
